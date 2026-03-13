adfeeee69efad6704563168d69c36243d25abdb8
YordiKings <reyesrojasjorge@gmail.com>
2026-03-13 04:24:35 +0000

    implement admin site

--- MyChessStats/WebChessStats/admin.py
+++ MyChessStats/WebChessStats/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date_played', 'white_player', 'black_player', 
+                   'result', 'opening', 'platform']
+    list_filter = ['platform', 'result', 'time_class', 'my_color', 'date_played']
+    search_fields = ['white_player', 'black_player', 'opponent_name', 'opening']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        ('Basic Info', {
+            'fields': ('platform', 'game_id', 'date_played', 'time_class')
+        }),
+        ('Players', {
+            'fields': ('white_player', 'black_player', 'my_color', 'opponent_name')
+        }),
+        ('Result', {
+            'fields': ('result', 'win_method', 'termination', 'outcome')
+        }),
+        ('Ratings', {
+            'fields': ('my_rating', 'opponent_rating', 'rating_change')
+        }),
+        ('Opening', {
+            'fields': ('eco', 'opening', 'opening_url')
+        }),
+        ('Game Data', {
+            'fields': ('fen', 'pgn', 'move_count', 'my_accuracy', 'opponent_accuracy')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at', 'is_active')
+        }),
+    )
--- MyChessStats/WebChessStats/apps.py
+++ MyChessStats/WebChessStats/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
-class WebchessstatsConfig(AppConfig):
+class WebChessStatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'WebChessStats'
